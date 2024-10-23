e14597454c52260d5a8161f32cad9b65e17d8947
Marleen <marleeneijkemans@gmail.com>
2024-10-23 22:32:01 +0200

    Create account page design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from "./login/login.component";
+import { CreateaccountComponent } from './createaccount/createaccount.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LoginComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginComponent, CreateaccountComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
